a9b6691303d24dfa2ae85a7cfabc5ff2c7e4f317
Shivani Gupta <helloshivani21apr@gmail.com>
2016-09-29 15:50:52 +0530

    Add msg to browser UI

--- public/js/app.js
+++ public/js/app.js
@@ -9,12 +9,13 @@
 console.log('New message:');
 console.log(message.text);
 
+jQuery('.messages').append('<p>' + message.text + '</p>');
  });
  //Handles submitting of new message
  var $form =jQuery('#message-form');
 
  $form.on('submit', function (event){
-     
+
 event.preventDefault();
 var $message = $form.find('input[name=message]');
 socket.emit('message', {
