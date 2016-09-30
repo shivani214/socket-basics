a84b45eae2fc077db790e0c910071e266a3e4d6d
Shivani Gupta <helloshivani21apr@gmail.com>
2016-09-30 15:06:07 +0530

    Show timestamp on UI

--- public/js/app.js
+++ public/js/app.js
@@ -6,10 +6,14 @@
  });
 
  socket.on('message', function (message){
+
+
+var momentTimestamp = moment.utc(message.timestamp);
+
 console.log('New message:');
 console.log(message.text);
 
-jQuery('.messages').append('<p>' + message.text + '</p>');
+jQuery('.messages').append('<p><strong>' + momentTimestamp.local().format('h:mm a') + ': </strong>' + message.text + '</p>');
  });
  //Handles submitting of new message
  var $form =jQuery('#message-form');
--- server.js
+++ server.js
@@ -3,21 +3,24 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var moment = require('moment');
 
 
-app.use(express.static(__dirname  + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function (socket){
+io.on('connection', function (socket) {
     console.log('User connected via socket.io!');
-socket.on('message', function (message){
-console.log('Message recieved: ' + message.text);
+    socket.on('message', function (message) {
 
-io.emit('message', message)
-});
+        console.log('Message recieved: ' + message.text);
+        message.timestamp = moment().valueOf();
+        io.emit('message', message)
+    });
     socket.emit('message', {
-        text: 'Welcome to the chat application!'
+        text: 'Welcome to the chat application!',
+        timestamp: moment().valueOf()
     });
-    });
+});
 
 http.listen(PORT, function () {
     console.log('Server started on PORT ' + PORT);
