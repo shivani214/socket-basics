c7fd3fb90dd17788ba2e9f6615bef3d669da194b
Shivani Gupta <helloshivani21apr@gmail.com>
2016-10-02 16:28:59 +0530

    add disconnect event and message

--- server.js
+++ server.js
@@ -7,20 +7,33 @@ var moment = require('moment');
 
 
 app.use(express.static(__dirname + '/public'));
-var clientInfo = {
+var clientInfo = {};
 
-};
 io.on('connection', function (socket) {
     console.log('User connected via socket.io!');
-    socket.on('joinRoom', function (req){
-        clientInfo[socket.id] = req;
-socket.join(req.room);
-socket.broadcast.to(req.room).emit('message',{
-    name: 'System',
-    text: req.name + ' has joined',
-    timestamp: moment().valueOf()
-});
+    socket.on('disconnect', function () {
+        var userData = clientInfo[socket.id];
+
+        if (typeof userData !== 'undefined') {
+            socket.leave(userData.room);
+            io.to(userData.room).emit('message', {
+                name: 'System',
+                text: userData.name + ' has left!',
+                timestamp: moment().valueOf()
+            });
+            delete clientInfo[socket.id];
+        }
     });
+    socket.on('joinRoom', function (req) {
+        clientInfo[socket.id] = req;
+        socket.join(req.room);
+        socket.broadcast.to(req.room).emit('message', {
+            name: 'System',
+            text: req.name + ' has joined',
+            timestamp: moment().valueOf()
+        });
+    });
+
     socket.on('message', function (message) {
 
         console.log('Message recieved: ' + message.text);
