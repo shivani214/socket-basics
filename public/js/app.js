bdc7461389d7f3e330c63da7dbc994cdd5be36d7
Shivani Gupta <helloshivani21apr@gmail.com>
2016-10-02 13:22:27 +0530

    Add join chat screen

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,37 @@
 var name = getQueryVariable('name') || 'Anonymous';
 var room = getQueryVariable('room');
- var socket = io();
+var socket = io();
 console.log(name + ' joined ' + room + ' room');
 
- socket.on('connect', function (){
-     console.log('Connected to socket.io server!');
+socket.on('connect', function () {
+    console.log('Connected to socket.io server!');
 
- });
-
- socket.on('message', function (message){
-
-
-var momentTimestamp = moment.utc(message.timestamp);
-var $message = jQuery('.messages');
-console.log('New message:');
-console.log(message.text);
-$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
-
-$message.append('<p>' + message.text + '</p>')
-  });
- //Handles submitting of new message
- var $form =jQuery('#message-form');
-
- $form.on('submit', function (event){
-
-event.preventDefault();
-var $message = $form.find('input[name=message]');
-socket.emit('message', {
-    name: name, 
-    text: $message.val()
 });
 
-$message.val(' ');
- });
+socket.on('message', function (message) {
+
+
+    var momentTimestamp = moment.utc(message.timestamp);
+    var $message = jQuery('.messages');
+    console.log('New message:');
+    console.log(message.text);
+    $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
+
+    $message.append('<p>' + message.text + '</p>')
+});
+//Handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+
+    event.preventDefault();
+    var $message = $form.find('input[name=message]');
+    socket.emit('message', {
+        name: name,
+        text: $message.val()
+    });
+
+    $message.val(' ');
+
+
+});
